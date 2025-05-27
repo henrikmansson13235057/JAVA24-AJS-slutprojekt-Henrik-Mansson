39a76c5a03b2593c6b6209607c330e18da1c70f2
Harry Koder <143705948+henrikmansson13235057@users.noreply.github.com>
2025-05-27 11:02:02 +0200

    Updates

---
+++ precache-manifest.1af5b1054e6a1c3c5582ca9d46ff3565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8bbe9e12fd14654f085103cad70ee75",
+    "url": "/JAVA24-AJS-slutprojekt-Henrik-Mansson/index.html"
+  },
+  {
+    "revision": "bece9a265c4dac1b5221",
+    "url": "/JAVA24-AJS-slutprojekt-Henrik-Mansson/static/css/main.8af81f39.chunk.css"
+  },
+  {
+    "revision": "ac3eaa1278b790b898d7",
+    "url": "/JAVA24-AJS-slutprojekt-Henrik-Mansson/static/js/2.00b84b78.chunk.js"
+  },
+  {
+    "revision": "bece9a265c4dac1b5221",
+    "url": "/JAVA24-AJS-slutprojekt-Henrik-Mansson/static/js/main.bbe805d8.chunk.js"
+  },
+  {
+    "revision": "0f5c6d9a97d8c892f19b",
+    "url": "/JAVA24-AJS-slutprojekt-Henrik-Mansson/static/js/runtime~main.4e2f93ed.js"
+  }
+]);
